6717fdd0210bcd3e2e019603b2d54ecc882a0cdb
qigaoqiang <qgqforu@163.com>
2022-03-19 00:12:16 +0800

    Update cloud-init.sh

--- 03-ECS实例初始化脚本/cloud-init.sh
+++ 03-ECS实例初始化脚本/cloud-init.sh
@@ -2,3 +2,15 @@
 
 yum install -y git
 yum install -y python36
+
+
+
+
+# 安装man手册
+yum install -y man-pages
+
+# 安装编译工具
+yum install -y gcc gcc-c++
+
+# 编译32位程序时(gcc -m32)
+yum install -y libgcc.i686
